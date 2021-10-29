64e5e05a207c2f66e3a2b2987573cda0380e0133
sourav1226 <souravghosh1226@gmail.com>
2021-10-29 16:59:36 +0530

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show(Todo $todo)
+    {
+        //
+    }
+
+ 
+    public function edit(Todo $todo)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Todo $todo)
+    {
+        //
+    }
+
+ 
+    public function destroy(Todo $todo)
+    {
+        //
+    }
+}
